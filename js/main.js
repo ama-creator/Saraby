9e178524e6884ecb831157dcdc559c7ad5e8d745
A M <markduho0@gmail.com>
2021-05-28 04:34:04 +0300

    a

--- js/main.js
+++ js/main.js
@@ -23,3 +23,36 @@ $(document).ready(function() {
 });
 
 
+// input range
+$( function() {
+    $( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 1500,
+      values: [ 0, 900 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+      }
+    });
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+  } );
+
+
+// /
+
+(function($) {
+    $(window).on('load', function() {
+        $('.mycustom-scroll').mCustomScrollbar();
+    });
+})(jQuery);
+
+$('.mycustom-scroll').mCustomScrollbar({
+    axis: 'y',              // вертикальный скролл
+    theme: 'rounded-dark',  // тема
+    scrollInertia: '330',   // продолжительность прокрутки, значение в миллисекундах
+    setHeight: '100%',      // высота блока (переписывает CSS)
+    mouseWheel: {
+        deltaFactor: 300    // кол-во пикселей на одну прокрутку колёсика мыши
+    }
+});
